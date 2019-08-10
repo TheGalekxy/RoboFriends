fc9b4097932cc30fd78bbdef7274d8f9be793fef
Adam Galek <Galekxy13@hotmail.com>
2019-08-10 00:50:46 -0400

    dom work

--- AndreiNeagoie/DOM/domscript.js
+++ AndreiNeagoie/DOM/domscript.js
@@ -1,67 +1,55 @@
-var button = document.getElementById("enter");
+var button = document.getElementById("enter");     /* sets the specific elements/id's to variables to make it more simple */
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 function inputLength() {
-	return input.value.length;
+    return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+	var li = document.createElement("li");      /* Creates an li element */ 
+	var btn = document.createElement("button"); // Creates a button and sets it to the variable "btn"
+	li.appendChild(document.createTextNode(input.value)); /* Adds the text to the previously created li */	
+	
+	li.addEventListener("click", function() {
+		var finished = this.classList.toggle("done"); 	// add the toggle of the "done" class to the newly created li
+	});
+
+	btn.appendChild(document.createTextNode("Delete")); //adds the text "Delete" to the button
+	li.appendChild(btn); //adds the Delete button to the end of the li
+	btn.addEventListener("click", function() {
+		btn.parentElement.remove();			// Deletes the parent element of the delete button(btn) which is the li associated with it
+	});
+
+	// btn.addEventListener("click", function() {
+	// 	li.classList.toggle("done");
+	// });
+
+	ul.appendChild(li); /* Adds the li to the end of the ul */
+	input.value = ""; /* Makes the input empty after adding the last input to the list */
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+    if (inputLength() > 0) { /* Makes sure the input to be added to the list is over 0 letters */
+        createListElement();
+    }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+    if (inputLength() > 0 && event.keyCode === 13) { /* Makes sure the input to be added to the list is over 0 letters and the key is enter/13 */
+        createListElement();
+    }
 }
 
 button.addEventListener("click", addListAfterClick);
-
 input.addEventListener("keypress", addListAfterKeypress);
 
-
-/* What I've written */ 
-var todosLength = document.querySelectorAll("li").length; //working
-var li = document.querySelectorAll("li");
-console.log(li)
-
-for (var i = 0; i < todosLength; i++) {
-	
-}
-
-function toggleAfterClick() {
-	if (li.classList.contains("done")) {
-		Document.querySelector(li).classList.toggle(done);
+function toggleClass(event) {
+	for (i = 0; i < li.length; i++) {
+		var li = document.getElementsByName("li");
+		li[i].addEventListener("click", toggleClass);
 	}
-	else { Document.querySelector(li).classList.toggle(done);	}
-	
-
-	
-}
-
-li.addEventListener("click", toggleAfterClick);
+	event.target.parentNode.classList.toggle("done");
+};
 
 
-
-
-// Buttons
-
-function addButtons() {
-
-}
-var deleteButton = document.createElement("button");
-
-button.innerHTML = "Delete";
-li.appendChild(button);
-
-// NEED TO CREATE A LOOP TO CHECK FOR AMOUNT OF li's to add on to!!
---
+++ Traversy/DOM Crash Course/dom.js
@@ -0,0 +1,93 @@
+// EXAMINE THE DOCUMENT OBJECT //
+
+// console.dir(document); Shows all the properties and methods attached to the document object
+// console.log(document.domain); Gives you the domain name
+// console.log(document.URL); Gives you the url
+// console.log(document.title); Gives you the title of the page
+// //document.title =  123; sets the title to "123"
+// console.log(document.doctype); shows the doctype 
+// console.log(document.head); show's the head and everything that is a child of head
+// console.log(document.body); show the body and all it's children
+// console.log(document.all); shows the whole document
+// console.log(document.all[10]); Show's the 10th element in the document
+// // document.all[10].textContent = 'Hello'; Changes the text content of the 10th element to "Hello" (not the way you want to select things)
+// console.log(document.forms[0]); Grabs the 1st form of the document
+// console.log(document.links); shows all the links in the document
+// console.log(document.images); show's all the images 
+
+//Selectors
+
+// GETELEMENTBYID //
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px #000';
+
+// GETELEMENTSBYCLASSNAME //
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+// // Gives error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < items.length; i++){
+//   items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+// GETELEMENTSBYTAGNAME //
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// // Gives error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < li.length; i++){
+//   li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+// QUERYSELECTOR //
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World'
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
+
+// QUERYSELECTORALL //
+// var titles = document.querySelectorAll('.title');
+
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even= document.querySelectorAll('li:nth-child(even)');
+
+// for(var i = 0; i < odd.length; i++){
+//   odd[i].style.backgroundColor = '#f4f4f4';
+//   even[i].style.backgroundColor = '#ccc';
+// }
+
