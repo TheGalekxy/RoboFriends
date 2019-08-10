e6e8b7b4d4d4fd151404629de6089e99f9a6b719
Adam Galek <Galekxy13@hotmail.com>
2019-08-10 00:50:56 -0400

    dom

--- Traversy/DOM Crash Course/dom.js
+++ Traversy/DOM Crash Course/dom.js
@@ -18,23 +18,23 @@
 //Selectors
 
 // GETELEMENTBYID //
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
+// console.log(document.getElementById('header-title')); getting the element by it's id of "header-title"
+// var headerTitle = document.getElementById('header-title'); adding the element pulled in the last line to a variable
+// var header = document.getElementById('main-header'); adds the element with id 'main-header' to the header variable
 // console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
+// headerTitle.textContent = 'Hello'; changing the text content of our new variable to "hello"
+// headerTitle.innerText = 'Goodbye'; cheanging the text content of our variable to "Goodbye", does not ignore STYLING
 // console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-// header.style.borderBottom = 'solid 3px #000';
+// headerTitle.innerHTML = '<h3>Hello</h3>'; edits the html to add an h3
+// header.style.borderBottom = 'solid 3px #000'; changes the border-bottom style of header to "solid 3px #000"
 
 // GETELEMENTSBYCLASSNAME //
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName('list-group-item'); sets all elements with the class name "list-group-item" to the variable 'items'
+// console.log(items); grabs all the items
+// console.log(items[1]); grabs the 2nd item in the variable
+// items[1].textContent = 'Hello 2'; changes the text content of the second item in the variable to 'Hello 2'
+// items[1].style.fontWeight = 'bold'; changes the font weight style of the second item in the variable to 'bold'
+// items[1].style.backgroundColor = 'yellow'; changes the background colour style of the second item in the variable to 'yellow
 
 // // Gives error
 // //items.style.backgroundColor = '#f4f4f4';
@@ -52,36 +52,36 @@
 // li[1].style.backgroundColor = 'yellow';
 
 // // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
+// //items.style.backgroundColor = '#f4f4f4'; DOES NOT WORK BECAUSE IT IS AN HTML COLLECTION, NOT AN ARRAY. WE HAVE TO LOOP THROUGH THE ITEMS TO DO THIS
 
 // for(var i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = '#f4f4f4';
+//   li[i].style.backgroundColor = '#f4f4f4'; Makes all the li have a black background
 // }
 
 // QUERYSELECTOR //
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
+// var header = document.querySelector('#main-header'); sets the elements with the selector #main-header to the variable header
+// header.style.borderBottom = 'solid 4px #ccc'; adds a border-bottom to the header var
 
-// var input = document.querySelector('input');
-// input.value = 'Hello World'
+// var input = document.querySelector('input'); setting the input to a variable
+// input.value = 'Hello World'; setting the input value through the variable
 
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
+// var submit = document.querySelector('input[type="submit"]'); selecting all the inputs that are submit type
+// submit.value="SEND"; setting the submit type to send.
 
-// var item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
+// var item = document.querySelector('.list-group-item'); 
+// item.style.color = 'red';        Gets the first item 
 
 // var lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color = 'blue';
+// lastItem.style.color = 'blue';  gets the last item
 
 // var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color = 'coral';
+// secondItem.style.color = 'coral'; selects the 2nd item in the list
 
 // QUERYSELECTORALL //
-// var titles = document.querySelectorAll('.title');
+// var titles = document.querySelectorAll('.title'); gets all the titles
 
 // console.log(titles);
-// titles[0].textContent = 'Hello';
+// titles[0].textContent = 'Hello'; changes the text content for the first title to hello
 
 // var odd = document.querySelectorAll('li:nth-child(odd)');
 // var even= document.querySelectorAll('li:nth-child(even)');
