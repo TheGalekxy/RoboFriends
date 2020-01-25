69a7e29343a7fb4e9ef627a4112346ca7547364e
Adam Galek <galekxy13@hotmail.com>
2020-01-24 20:35:18 -0500

    creating my first React Component

---
+++ React/robo-friends/src/Background.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Background.css';
+
+class Background extends Component {
+    render() {
+        return (
+        <div className='f1 tc'>
+            <h1> Hello World! </h1>             {/*This is JSX, not html*/}
+            <p> This is going to change the background colour to *Blue*</p>
+        </div>
+        )
+    }
+}
+
+export default Background
