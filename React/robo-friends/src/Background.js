a69c205789a309e838c1ff613f1c6cda80097c97
Adam Galek <galekxy13@hotmail.com>
2020-01-29 11:47:31 -0500

    clearing

--- React/robo-friends/src/Background.js
+++ React/robo-friends/src/Background.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './Background.css';
 
 class Background extends Component {
     render() {
         return (
-        <div className='f1 tc'>
+        <Fragment className='f1 tc'>
             <h1> Hello World! </h1>             {/*This is JSX, not html*/}
             <p> This is going to change the background colour to a *gradient* </p>
-        </div>
+        </Fragment>
         )
     }
 }
