b04b66b6fa6f210f6f286e0088d80811679f4e63
Adam Galek <galekxy13@hotmail.com>
2020-01-31 13:22:47 -0500

    cleaning up

--- AndreiNeagoie/Advanced Javascript/TypeCoercion.js
+++
@@ -1,8 +0,0 @@
-// Type coercion means that when the opperants (left and right of operator (=)) are different types one of them will be converted into an equivilant value.
-// eg. 
-1 == '1'
-
-// type coercion happens when you use the double equal ( == ).
-// It means compare the two values and if their types are different coerce them into the same type
-
-// === means compare two values but don't try and change their types, be explicit
