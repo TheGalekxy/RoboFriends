64841358366710d19e4139884524e224e6458142
Adam Galek <galekxy13@hotmail.com>
2020-02-12 12:20:44 -0500

    removed redundant files

--- Advanced Javascript/Debugging.js
+++
@@ -1,9 +0,0 @@
-const flattened = [[0,1], [2,3], [4,5]].reduce( 
-    (a, b) => a.concat(b), []);
-
-const flattened = [[0,1], [2,3], [4,5]].reduce( 
-    (accumulator, b) => accumulator.concat(b), []);
-
-const flattened = [[0,1], [2,3], [4,5]].reduce( 
-    (accumulator, array) => accumulator.concat(array), []);  // array is going to be empty as we start off
-
--- ElequentJavascript/Chapter 2/LoopingTriangle.js
+++
@@ -1,5 +0,0 @@
-const result = ""
-for (let counter = 0; counter < 7; counter++ ) {
-    console.log(result + '#')
-}
-console.log(result)
